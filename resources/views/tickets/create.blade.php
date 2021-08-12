46f3dddf918ce110a059e80df401d2ef15e479f2
joel goyo <joelgoyosaez@gmail.com>
2021-08-12 18:43:38 -0400

    edit

--- resources/views/tickets/create.blade.php
+++ resources/views/tickets/create.blade.php
@@ -1,11 +1,9 @@
 @extends('layouts.dashboard')
 
  @section('content')
-
-
  <section id="basic-vertical-layouts">
      <div class="row match-height d-flex justify-content-center">
-         <div class="col-md-6 col-12">
+         <div class="col-md-12 col-12">
              <div class="card bg-lp">
                  <div class="card-header">
                      <h4 class="card-title text-white">Creacion de Ticket</h4>
@@ -16,15 +14,12 @@
                              @csrf
                              <div class="form-body">
                                  <div class="row">
-
                                      <div class="col-12">
                                          <label class="form-label text-white mb-1" for="issue"><b>Asunto del
                                                  ticket</b></label>
                                          <input class="form-control border border-primary rounded-0" required type="text"
                                              id="issue" name="issue" rows="3" />
-
                                      </div>
-
                                      <div class="col-12 mt-2">
                                          <div class="form-group">
                                              <div class="controls">
@@ -42,19 +37,14 @@
                                              </div>
                                          </div>
                                      </div>
-
                                      <div class="col-12 mt-2 mb-2">
                                          <label class="form-label text-white mb-1" for="message"><b>Chat con el
                                                  administrador</b></label>
-
                                          <section class="chat-app-window mb-2 border border-primary rounded-0">
                                              <div class="active-chat">
                                                  <div class="user-chats ps ps--active-y bg-lp">
                                                      <div class="chats chat-thread">
-
-                                    
                                                             {{-- admin --}}
-
                                                          <div class="chat chat-left">
                                                              <div class="chat-avatar">
                                                                  <span class="avatar box-shadow-1 cursor-pointer">
@@ -68,36 +58,29 @@
                                                                  </div>
                                                              </div>
                                                          </div>
-
-
                                                    </div>
                                                  </div>
                                              </div>
                                          </section>
-
                                          <br>
                                         <span class="text-danger text-bold-600">Aqui podra escribir el mensaje para el admin</span>
                                          <textarea
                                              class="form-control border border-primary rounded-0 chat-window-message"
                                              type="text" id="message" name="message" required rows="3"></textarea>
-                                     </div>
+                                        </div>
                                      <div class="col-12">
                                          <button type="submit"
                                              class="btn btn-primary mr-1 mb-1 waves-effect waves-light">Enviar
                                              Ticket</button>
                                      </div>
                                  </div>
-
                              </div>
-                     </div>
+                        </div>
                      </form>
                  </div>
              </div>
          </div>
-     </div>
-     </div>
+    </div>
+</div>
  </section>
-
-
-
  @endsection
