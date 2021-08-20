02f71a3f22bd6b715c2533813f9359ff6a9c6002
joelgoyo <65769355+joelgoyo@users.noreply.github.com>
2021-08-20 09:48:09 -0400

    edit a about

---
+++ resources/views/backofice/contact_us.blade.php
@@ -0,0 +1,17 @@
+@extends('backofice.layouts.dashboard')
+
+@section('content')
+<div class="" style="background: #173138">
+    <div class="texto-tienda">
+        <strong>contáctanos</strong>
+     </div>
+     <div class="texto-tiendaB">
+        <p>Inicio <strong> > </strong>Tienda</p>
+      </div>
+    <img src="{{asset('assets/img/home/formas_fondo3.png')}}" alt=""  style="width: 100%;">
+</div>
+    
+<div class="container">
+    <div class="row"></div>
+</div>
+@endsection
