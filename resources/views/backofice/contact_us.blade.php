d11ee98e9c840b6fefbc44637d5812e05b8d0366
joelgoyo <65769355+joelgoyo@users.noreply.github.com>
2021-09-01 12:50:15 -0400

    edit

--- resources/views/backofice/contact_us.blade.php
+++ resources/views/backofice/contact_us.blade.php
@@ -95,7 +95,7 @@ input[type=text]:hover:not([disabled]), input[type=text]:focus, input[type=email
 <div class="pl-2 pr-4 pt-3">
 <div class="row d-flex justify-content-center">
 <div class="col-md-7 mt-5 mb-5">
-
+ 
     <div class="card p-5" style="height:450px;background: #ffffff;">
         <form action={{route('contact')}} method="POST" class="credit-card-div">
             {{ csrf_field() }}
