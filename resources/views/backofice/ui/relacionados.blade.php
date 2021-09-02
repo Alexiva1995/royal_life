9b34d1d2608438cd4ee7fc41fd1c2068daac5714
juliozn5 <Juliocesarzn5@gmail.com>
2021-09-02 17:39:11 -0400

    detalle de producto retoques

--- resources/views/backofice/ui/relacionados.blade.php
+++ resources/views/backofice/ui/relacionados.blade.php
@@ -5,7 +5,8 @@
 <div class="ml-1 row d-flex  mt-2">
     <div class="card-body shadow ml-2 zoom2 mover4" style="background: white;border-radius: 10px; left: 700px; 10px;padding: 0.8rem;">
         <div class="d-flex ">
-            <div class="background ">
+            <a class="background "
+                 href="{{action('TiendaController@detalleproducto', ['producto' => $item->id ])}}">
                 @if($producto->img == null)
                              <img class="mx-auto d-block  img-fluid"
                               src="{{asset('assets/img/home/producto21.png')}}"
@@ -17,17 +18,14 @@
                                  alt="Product Image"
                                  style="min-width:90%; ">
                       @endif
-            </div>
+        </a>
             <div class="col-6 ">
                 <h5><strong>{{$item->name}}</strong></h5>
                 <p class="card-text" style="color: black; width: 160px;">
                     {{ Str::words( strip_tags($producto->description) , 9 )}}</p>
-                <a href="" class="btn btn-cre">
-                    <p style="margin-top: -9px; margin-left: -13px;">{{ ucfirst($item->getCategories->categories_name)}}</p>
-                </a>
-                <p
-                    style="color: black;margin-left: 140px;font-size: 20px;margin-bottom: 0;margin-top: -5px;">
-                    <strong>${{$item->price}}</strong></p>
+
+                <p class="text-right" style="color: rgb(0, 0, 0);"><a class="btn-cu text-center ">{{ ucfirst($producto->getCategories->categories_name)}}</a><strong class="texto3  prize2">${{$producto->price}}</strong></p>
+
             </div>
         </div>
     </div>
