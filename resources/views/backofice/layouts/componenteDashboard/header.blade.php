9742bd59f357d891c8df9ddb442ec9ab5ac8263f
joelgoyo <65769355+joelgoyo@users.noreply.github.com>
2021-09-06 14:02:22 -0400

    edit

--- resources/views/backofice/layouts/componenteDashboard/header.blade.php
+++ resources/views/backofice/layouts/componenteDashboard/header.blade.php
@@ -102,7 +102,7 @@
                     </li>
                 </ul>
                 @endif
-              <a class="text-white pr-1"style="font-size: 25px;" href="">|</a>
+              <a class="text-white"style="font-size: 25px;" href="">|</a>
             <a href="{{route('cart')}}"><img src="{{asset('assets/img/iconnew/Group.png')}}" alt=""></a>
     </div>
 
